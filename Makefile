93caba8367c6a29d761eda067e15a21164ca62aa
Werner Van Geit <werner.vangeit@epfl.ch>
2016-02-15 14:51:30 +0100

    Added makefile and setup.py

---
+++ Makefile
@@ -0,0 +1,32 @@
+all: install
+install:
+	pip install . --upgrade --force-reinstall
+doc: install
+	pip install sphinx sphinx-autobuild
+	cd docs; $(MAKE) clean; $(MAKE) html
+docopen: doc
+	open docs/build/html/index.html
+docpdf: install
+	pip install sphinx sphinx-autobuild
+	cd docs; $(MAKE) clean; $(MAKE) latexpdf
+test: install
+	pip install nose
+	# TODO create one big test for this
+	nosetests -s -v -x -w bluepyopt/tests/
+	cd examples/l5pc; nrnivmodl mechanisms;  	
+	nosetests -s -v -x -w examples/tests/
+example: install
+	cd examples/simplecell && \
+	python ./opt_simplecell.py
+l5pc_start: install
+	cd examples/l5pc && \
+	nrnivmodl mechanisms && \
+	python ./opt_l5pc.py --start
+l5pc_cont: install
+	cd examples/l5pc && \
+	nrnivmodl mechanisms && \
+	python ./opt_l5pc.py --continue_cp
+l5pc_analyse: install
+	cd examples/l5pc && \
+	nrnivmodl mechanisms && \
+	python ./opt_l5pc.py --analyse
---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""BluePyOpt  setup """
+
+"""
+Copyright (c) 2015, EPFL/Blue Brain Project
+
+ This file is part of eFEL <https://github.com/BlueBrain/BluePyOpt>
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License version 3.0 as published
+ by the Free Software Foundation.
+
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+import setuptools
+
+# try:
+#     from setuptools import setup, Extension
+# except ImportError:
+# from distutils.core import setup, Extension  # pylint: disable=E0611,F0401
+
+setuptools.setup(
+    name="bluepyopt",
+    version="0.0.1",
+    # install_requires=['numpy>=1.6'],
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    author="BlueBrain Project, EPFL",
+    author_email="werner.vangeit@epfl.ch",
+    description="Bluebrain Python Optimisation Library (bluepyopt)",
+    long_description="Bluebrain Python Optimisation Library (bluepyopt)",
+    license="LGPLv3",
+    keywords=(
+        'optimisation',
+        'neuroscience',
+        'BlueBrainProject'),
+    url='https://github.com/BlueBrain/BluePyOpt',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU Lesser General Public '
+        'License v3 (LGPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: POSIX',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Utilities'],
+    package_data={})
