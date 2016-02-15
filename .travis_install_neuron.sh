7caa0a5342456b97b0bbfc22df7c5851a95f0fba
Werner Van Geit <werner.vangeit@epfl.ch>
2016-02-15 19:12:34 +0100

    Prevent rebuilding of travis Neuron if it was compiled in previous build

--- .travis_install_neuron.sh
+++ .travis_install_neuron.sh
@@ -4,15 +4,22 @@ set -ex
 
 INSTALL_DIR=$1
 
-mkdir -p src
-cd src
-wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
-tar xzvf nrn-7.4.tar.gz
-cd nrn-7.4
-./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython have_cython=no BUILD_RX3D=0
-make -j4 install
+if [ ! -e ${INSTALL_DIR}/.install_finished ]
+then
+    mkdir -p src
+    cd src
+    wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
+    tar xzvf nrn-7.4.tar.gz
+    cd nrn-7.4
+    ./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython have_cython=no BUILD_RX3D=0
+    make -j4 install
 
-export PATH="${INSTALL_DIR}/x86_64/bin":${PATH}
-export PYTHONPATH="${INSTALL_DIR}/lib/python":${PYTHONPATH}
+    export PATH="${INSTALL_DIR}/x86_64/bin":${PATH}
+    export PYTHONPATH="${INSTALL_DIR}/lib/python":${PYTHONPATH}
 
-python -c 'import neuron'
+    python -c 'import neuron'
+
+    touch -f ${INSTALL_DIR}/.install_finished
+else
+    echo 'Neuron was fully installed in previous build, not rebuilding'
+fi
