539bedd2de69f94a499469a819145f5c049694a3
Werner Van Geit <werner.vangeit@epfl.ch>
2016-02-15 16:58:15 +0100

    Disable interviews in travis neuron install

--- .travis_install_neuron.sh
+++ .travis_install_neuron.sh
@@ -7,5 +7,5 @@ cd src
 wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
 tar xzvf nrn-7.4.tar.gz
 cd nrn-7.4
-./configure --prefix=$HOME/local/nrn
+./configure --prefix=$HOME/local/nrn --without-x
 make -j4 install
