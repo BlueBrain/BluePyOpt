778ec9eb12463f8dfb2fd0717c04aa6d82c28dbb
Werner Van Geit <werner.vangeit@epfl.ch>
2016-02-15 17:03:47 +0100

    Added PATH and PYTHONPATH to neuron travis install

--- .travis_install_neuron.sh
+++ .travis_install_neuron.sh
@@ -2,10 +2,15 @@
 
 set -ex
 
+INSTALL_DIR=$HOME/local/nrn
+
 mkdir -p src
 cd src
 wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
 tar xzvf nrn-7.4.tar.gz
 cd nrn-7.4
-./configure --prefix=$HOME/local/nrn --without-x
+./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython
 make -j4 install
+
+export PATH="${INSTALL_DIR}/x86_64/bin":${PATH}
+export PYTHONPATH="${INSTALL_DIR}/lib/python":${PYTHONPATH}
