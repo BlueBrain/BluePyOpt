0df66118a0b21b39312371b4c0a1e1177b26ab2c
Werner Van Geit <werner.vangeit@epfl.ch>
2016-04-28 15:07:49 +0200

    Fixes in NSG portal scripts

--- examples/l5pc/nsg/init.py
+++ examples/l5pc/nsg/init.py
@@ -6,8 +6,6 @@ os.system('pip install efel --user')
 
 os.system('pip install bluepyopt --upgrade --user')
 
-os.system('nrnivmodl mechanisms')
+# os.system('nrnivmodl mechanisms')
 
-import sys
-sys.argv = ['opt_l5pc.py', '--start']
-os.system('./opt_l5pc.py --start')
+os.system('python opt_l5pc.py --start')
