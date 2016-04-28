0548102ed8d4314c07c21fd502404027809c94c6
C Rössert <christian.rossert@epfl.ch>
2016-04-28 12:56:26 +0200

    Add master dt and cvode_active to simulator class

--- bluepyopt/ephys/simulators.py
+++ bluepyopt/ephys/simulators.py
@@ -12,7 +12,7 @@ class NrnSimulator(object):
 
     """Neuron simulator"""
 
-    def __init__(self):
+    def __init__(self, dt=0.025, cvode_active=True):
         """Constructor"""
 
         hoc_so = os.path.join(imp.find_module('neuron')[1] + '/hoc.so')
@@ -22,17 +22,34 @@ class NrnSimulator(object):
 
         import neuron  # NOQA
 
+        self.dt = dt
+        self.cvode_active = cvode_active
+
     @property
     def neuron(self):
         """Return neuron module"""
         import neuron
         return neuron
 
-    def run(self, tstop=None, cvode_active=True):
+    def run(self, tstop=None, cvode_active=None, dt=None):
         """Run protocol"""
 
         self.neuron.h.tstop = tstop
+
+        if cvode_active is None: # use cvode_active of simulator
+            cvode_active = self.cvode_active
+
         self.neuron.h.cvode_active(1 if cvode_active else 0)
-        logger.debug('Running Neuron simulator %.6g ms', tstop)
+
+        if dt is None: # use dt of simulator
+            dt = self.dt
+
+        if cvode_active:
+            logger.debug('Running Neuron simulator %.6g ms, with cvode', tstop)
+        else:
+            self.neuron.h.dt = dt
+            self.neuron.h.steps_per_ms = 1.0 / dt
+            logger.debug('Running Neuron simulator %.6g ms, with dt=%r', tstop, dt)
+
         self.neuron.h.run()
         logger.debug('Neuron simulation finished')
