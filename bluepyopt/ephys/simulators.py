9df3dc93cca0356fb1fb51a933fe4f32936923ae
Werner Van Geit <werner.vangeit@epfl.ch>
2016-05-02 12:50:46 +0200

    Added some safety checks in case user specifies e.g. cvode_active AND dt

--- bluepyopt/ephys/simulators.py
+++ bluepyopt/ephys/simulators.py
@@ -12,7 +12,7 @@ class NrnSimulator(object):
 
     """Neuron simulator"""
 
-    def __init__(self, dt=0.025, cvode_active=True):
+    def __init__(self, dt=None, cvode_active=True):
         """Constructor"""
 
         hoc_so = os.path.join(imp.find_module('neuron')[1] + '/hoc.so')
@@ -22,9 +22,12 @@ class NrnSimulator(object):
 
         import neuron  # NOQA
 
-        self.dt = dt
+        self.dt = dt if self.dt is not None else neuron.h.dt
         self.cvode_active = cvode_active
 
+    # pylint: disable=R0201
+    # TODO function below should probably a class property or something in that
+    # sense
     @property
     def neuron(self):
         """Return neuron module"""
@@ -36,12 +39,23 @@ class NrnSimulator(object):
 
         self.neuron.h.tstop = tstop
 
-        if cvode_active is None: # use cvode_active of simulator
+        if cvode_active and dt is not None:
+            raise ValueError(
+                'NrnSimulator: Impossible to combine the dt argument when '
+                'cvode_active is True in the NrnSimulator run method')
+
+        if cvode_active is None:
             cvode_active = self.cvode_active
 
         self.neuron.h.cvode_active(1 if cvode_active else 0)
 
-        if dt is None: # use dt of simulator
+        if dt is None:  # use dt of simulator
+            if self.neuron.h.dt != self.dt:
+                raise Exception(
+                    'NrnSimulator: Some process has changed the '
+                    'time step dt of Neuron since the creation of this '
+                    'NrnSimulator object. Not sure this is intended, '
+                    'raising Exception')
             dt = self.dt
 
         if cvode_active:
@@ -49,7 +63,10 @@ class NrnSimulator(object):
         else:
             self.neuron.h.dt = dt
             self.neuron.h.steps_per_ms = 1.0 / dt
-            logger.debug('Running Neuron simulator %.6g ms, with dt=%r', tstop, dt)
+            logger.debug(
+                'Running Neuron simulator %.6g ms, with dt=%r',
+                tstop,
+                dt)
 
         self.neuron.h.run()
         logger.debug('Neuron simulation finished')
