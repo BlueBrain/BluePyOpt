622b76b7c0da9d4a8519ec0c88471fe148535602
Werner Van Geit <werner.vangeit@epfl.ch>
2016-05-03 17:07:04 +0200

    Add test for NrnFileMorphology set_nseg() can now be disabled in NrnFileMorphology constructor

--- bluepyopt/tests/test_ephys/test_morphologies.py
+++ bluepyopt/tests/test_ephys/test_morphologies.py
@@ -1,9 +1,19 @@
 """ephys/morphologies.py unit tests"""
 
+import os
+
 import nose.tools as nt
 from nose.plugins.attrib import attr
 
 import bluepyopt.ephys as ephys
+
+testdata_dir = os.path.join(
+    os.path.dirname(
+        os.path.abspath(__file__)),
+    'testdata')
+
+simpleswc_morphpath = os.path.join(testdata_dir, 'simple.swc')
+simplewrong_morphpath = os.path.join(testdata_dir, 'simple.wrong')
 
 
 @attr('unit')
@@ -12,3 +22,23 @@ def test_morphology_init():
 
     morph = ephys.morphologies.Morphology()
     nt.assert_is_instance(morph, ephys.morphologies.Morphology)
+
+
+@attr('unit')
+def test_nrnfilemorphology_init():
+    """ephys.morphologies: testing NrnFileMorphology constructor"""
+    sim = ephys.simulators.NrnSimulator()
+
+    # morph = ephys.morphologies.NrnFileMorphology(simple_morphpath)
+    # nt.assert_is_instance(morph, ephys.morphologies.NrnFileMorphology)
+
+    morph = ephys.morphologies.NrnFileMorphology('wrong.swc')
+    nt.assert_raises(IOError, morph.instantiate, sim=sim)
+
+    morph = ephys.morphologies.NrnFileMorphology(simplewrong_morphpath)
+    nt.assert_raises(ValueError, morph.instantiate, sim=sim)
+
+    morph = ephys.morphologies.NrnFileMorphology(
+        simpleswc_morphpath,
+        do_set_nseg=False)
+    morph.instantiate(sim=sim)
