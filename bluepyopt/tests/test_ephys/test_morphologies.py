8a8f5ad2dfa415fa1b8a08cb96cd343f48f29b17
Werner Van Geit <werner.vangeit@epfl.ch>
2016-05-03 13:35:58 +0200

    Added nosetest for Stimulus and Morphology init

--- bluepyopt/tests/test_ephys/test_morphologies.py
+++ bluepyopt/tests/test_ephys/test_morphologies.py
@@ -1,5 +1,7 @@
 """ephys/morphologies.py unit tests"""
 
+import nose.tools as nt
+
 import bluepyopt.ephys as ephys
 
 
@@ -7,3 +9,4 @@ def test_morphology_init():
     """ephys.morphologies: testing Morphology constructor"""
 
     morph = ephys.morphologies.Morphology()
+    nt.assert_is_instance(morph, ephys.morphology.Morphology)
