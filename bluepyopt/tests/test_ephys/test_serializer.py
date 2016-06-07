00a97542cd3e2da59f75a3c45aa4b1708a49e8ef
Werner Van Geit <werner.vangeit@epfl.ch>
2016-06-07 13:17:53 +0200

    Add serializer test to fast unit tests class, fixes in these tests

--- bluepyopt/tests/test_ephys/test_serializer.py
+++ bluepyopt/tests/test_ephys/test_serializer.py
@@ -1,11 +1,17 @@
+"""Test for ephys.serializer"""
+
 import json
 import nose.tools as nt
+from nose.plugins.attrib import attr
 
-from bluepyopt.ephys.serializer import DictMixin, instantiator, SENTINAL
+import bluepyopt.ephys as ephys
 
 
-class TestClass(DictMixin):
+class TestClass(ephys.serializer.DictMixin):
+
+    """Test class for serializer"""
     SERIALIZED_FIELDS = ('string', 'boolean', 'float_', 'list_', 'dict_')
+
     def __init__(self, string, boolean, float_, list_, dict_):
         self.string = string
         self.boolean = boolean
@@ -14,40 +20,56 @@ class TestClass(DictMixin):
         self.dict_ = dict_
 
 
+class NestedTestClass(ephys.serializer.DictMixin):
+
+    """Nested test class for serializer"""
+
+    SERIALIZED_FIELDS = ('test', 'tuples', 'lists', 'dicts', )
+
+    def __init__(self, test, tuples, lists, dicts):
+        self.test = test
+        self.tuples = tuples
+        self.lists = lists
+        self.dicts = dicts
+
+
+@attr('unit')
 def test_serializer():
+    """ephys.serializer: test serialization of test class"""
     tc = TestClass('some string', False, 1.0, [1, 2, 3], {'0': 0})
     serialized = tc.to_dict()
     nt.ok_(isinstance(serialized, dict))
-    dumps = json.dumps(serialized)
+    json.dumps(serialized)
 
 
+@attr('unit')
 def test_roundtrip_serializer():
+    """ephys.serializer: test round trip of serialization of test class"""
+
     tc = TestClass('some string', False, 1.0, [1, 2, 3], {'0': 0})
     serialized = tc.to_dict()
-    instantiated = instantiator(serialized)
+    instantiated = ephys.serializer.instantiator(serialized)
     nt.ok_(isinstance(instantiated, TestClass))
 
 
+@attr('unit')
 def test_nested_serializer():
-    class NestedTestClass(DictMixin):
-        SERIALIZED_FIELDS = ('test', 'tuples', 'lists', 'dicts', )
-        def __init__(self, test, tuples, lists, dicts):
-            self.test = test
-            self.tuples = tuples
-            self.lists = lists
-            self.dicts = dicts
+    """ephys.serializer: test a nested serialization of test class"""
 
     tc = TestClass('some string', False, 1.0, [1, 2, 3], {'0': 0})
-    ntc = NestedTestClass(test=tc, tuples=(tc, ), lists=[tc]*3, dicts={0: tc})
+    ntc = NestedTestClass(test=tc, tuples=(tc, ), lists=[tc] * 3, dicts={0: tc})
     serialized = ntc.to_dict()
-    dumps = json.dumps(serialized, indent=2)
+    json.dumps(serialized, indent=2)
 
-    instantiated = instantiator(serialized)
+    instantiated = ephys.serializer.instantiator(serialized)
     nt.ok_(isinstance(instantiated, NestedTestClass))
     nt.ok_(isinstance(instantiated.lists[0], TestClass))
     nt.ok_(isinstance(instantiated.dicts[0], TestClass))
 
 
+@attr('unit')
 @nt.raises(Exception)
-def test_non_instatiable():
-    instantiator({'some': 'fake', 'class': SENTINAL, })
+def test_non_instantiable():
+    """ephys.serializer: test non instantiable class"""
+    ephys.serializer.instantiator(
+        {'some': 'fake', 'class': ephys.serializer.SENTINAL, })
