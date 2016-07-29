675f97195b397c69b2b5d960c082766a633fd242
Mike Gevaert <michael.gevaert@epfl.ch>
2016-07-29 09:35:13 +0200

    mssing test.utils file

---
+++ bluepyopt/tests/test_ephys/utils.py
@@ -0,0 +1,29 @@
+from bluepyopt import ephys
+from bluepyopt.ephys.parameters import (NrnGlobalParameter, NrnSectionParameter,
+                                        NrnRangeParameter, )
+from bluepyopt.ephys.locations import NrnSeclistLocation
+
+
+def make_mech():
+    basal = ephys.locations.NrnSeclistLocation('basal', seclist_name='basal')
+    apical = ephys.locations.NrnSeclistLocation('apical', seclist_name='apical')
+    return ephys.mechanisms.NrnMODMechanism('Ih', prefix='Ih', locations=[basal, apical, ])
+
+
+def make_parameters():
+    value, frozen, bounds, param_name = 65, False, [0, 100.0], 'gSKv3_1bar_SKv3_1'
+    value_scaler = ephys.parameterscalers.NrnSegmentLinearScaler()
+    locations = (NrnSeclistLocation('Location0', 'somatic'),
+                 NrnSeclistLocation('Location1', 'apical'),
+                 )
+    parameters = (
+        NrnGlobalParameter('NrnGlobalParameter', value, frozen, bounds, param_name),
+        NrnSectionParameter(
+            'NrnSectionParameter', value, frozen, bounds, param_name, value_scaler, locations),
+        NrnRangeParameter(
+            'NrnRangeParameter', value, frozen, bounds, param_name, value_scaler, locations),
+    )
+    return parameters
+
+
+
