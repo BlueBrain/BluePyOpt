094268e4680d2b6f96bf89e2f51b81467e421fa7
Mike Gevaert <michael.gevaert@epfl.ch>
2016-07-29 09:25:51 +0200

    Create NEURON hoc files from ephys models
    * initial version, considered beta
    * given a set of mechanisms, and frozen parameters, will
      generate a hoc cell template that can be loaded in neuron,
      and instantiated with a morphology.
    * Much of the template hoc code comes from
        https://bbp.epfl.ch/nmc-portal/downloads
      But was refactred to be self contained and simpler

--- bluepyopt/tests/test_ephys/test_mechanisms.py
+++ bluepyopt/tests/test_ephys/test_mechanisms.py
@@ -1,16 +1,14 @@
 import json
-from os.path import join as joinp
 
 import nose.tools as nt
 
+import utils
 from bluepyopt import ephys
 from bluepyopt.ephys.serializer import instantiator
 
 
 def test_mechanism_serialize():
-    basal = ephys.locations.NrnSeclistLocation('basal', seclist_name='basal')
-    apical = ephys.locations.NrnSeclistLocation('apical', seclist_name='apical')
-    mech = ephys.mechanisms.NrnMODMechanism('Ih', prefix='Ih', locations=[basal, apical, ])
+    mech = utils.make_mech()
     serialized = mech.to_dict()
     nt.ok_(isinstance(json.dumps(serialized), str))
     deserialized = instantiator(serialized)
