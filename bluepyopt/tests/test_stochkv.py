001fcf4756b096ae522a1475ce96ab42373428fa
Werner Van Geit <werner.vangeit@epfl.ch>
2016-10-03 17:26:00 +0200

    Removed _remove_first_line from stochkv hoc comparison

--- bluepyopt/tests/test_stochkv.py
+++ bluepyopt/tests/test_stochkv.py
@@ -25,12 +25,6 @@ def test_import():
     import stochkvcell  # NOQA
 
 
-def _remove_first_line(string):
-    """Remove first line of string"""
-
-    return '\n'.join(string.split('\n')[1:])
-
-
 def test_run():
     """StochKv example: test run"""
 
@@ -53,76 +47,5 @@ def test_run():
         expected_hoc_string = expected_hoc_file.read()
 
     nt.assert_equal(
-        _remove_first_line(hoc_string),
-        _remove_first_line(expected_hoc_string))
-
-'''
-class TestL5PCModel(object):
-
-    """Test L5PC model"""
-
-    def __init__(self):
-        self.l5pc_cell = None
-        self.nrn = None
-
-    def setup(self):
-        """Set up class"""
-        sys.path.insert(0, L5PC_PATH)
-
-        import l5pc_model  # NOQA
-        self.l5pc_cell = l5pc_model.create()
-        nt.assert_is_instance(
-            self.l5pc_cell,
-            bluepyopt.ephys.models.CellModel)
-        self.nrn = ephys.simulators.NrnSimulator()
-
-    def test_instantiate(self):
-        """L5PC: test instantiation of l5pc cell model"""
-        self.l5pc_cell.freeze(release_parameters)
-        self.l5pc_cell.instantiate(sim=self.nrn)
-
-    def teardown(self):
-        """Teardown"""
-        self.l5pc_cell.destroy(sim=self.nrn)
-
-
-class TestL5PCEvaluator(object):
-
-    """Test L5PC evaluator"""
-
-    def __init__(self):
-        self.l5pc_evaluator = None
-
-    def setup(self):
-        """Set up class"""
-
-        import l5pc_evaluator  # NOQA
-
-        self.l5pc_evaluator = l5pc_evaluator.create()
-
-        nt.assert_is_instance(
-            self.l5pc_evaluator,
-            bluepyopt.ephys.evaluators.CellEvaluator)
-
-    @attr('slow')
-    def test_eval(self):
-        """L5PC: test evaluation of l5pc evaluator"""
-
-        result = self.l5pc_evaluator.evaluate_with_dicts(
-            param_dict=release_parameters)
-
-        expected_results = load_from_json('expected_results.json')
-
-        # Use two lines below to update expected result
-        # expected_results['TestL5PCEvaluator.test_eval'] = result
-        # dump_to_json(expected_results, 'expected_results.json')
-
-        nt.assert_items_equal(
-            result,
-            expected_results['TestL5PCEvaluator.test_eval'])
-
-    def teardown(self):
-        """Teardown"""
-        pass
-
-'''
+        hoc_string,
+        expected_hoc_string)
