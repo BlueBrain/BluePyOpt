2fd5d37701c0e11027e107ec7de4a8a74f1eec68
Werner Van Geit <werner.vangeit@epfl.ch>
2016-10-25 17:03:37 +0200

    Make uint32_seed py3 compatible

--- bluepyopt/tools.py
+++ bluepyopt/tools.py
@@ -6,6 +6,6 @@ import hashlib
 def uint32_seed(string):
     """Get unsigned int seed of a string"""
 
-    hex_value = hashlib.md5(string).hexdigest()
+    hex_value = hashlib.md5(string.encode('utf-8')).hexdigest()
 
     return int(hex_value, 16) & 0xFFFFFFFF
