12f6d6b867b7e146d881d9c1a872ba6f4daaef17
Werner Van Geit <werner.vangeit@epfl.ch>
2018-11-20 16:20:04 +0100

    Get python bin from tox

--- .install_neuron.sh
+++ .install_neuron.sh
@@ -4,6 +4,7 @@ set -e
 
 SRC_DIR=$1
 INSTALL_DIR=$2
+PYTHON_BIN=$3
 
 if [ ! -e ${INSTALL_DIR}/.install_finished ]
 then
@@ -11,12 +12,13 @@ then
     mkdir -p ${SRC_DIR}
     cd ${SRC_DIR}
     echo "Downloading NEURON ..."
+    rm -rf nrn
 	git clone --depth 1 https://github.com/nrnhines/nrn.git >download.log 2>&1
 	cd nrn
     echo "Preparing NEURON ..."
 	./build.sh >prepare.log 2>&1
     echo "Configuring NEURON ..."
-    ./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython --disable-rx3d >configure.log 2>&1
+    ./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython=${PYTHON_BIN} --disable-rx3d >configure.log 2>&1
     echo "Building NEURON ..."
     make -j4 >make.log 2>&1
     echo "Installing NEURON ..."
@@ -26,7 +28,7 @@ then
     export PYTHONPATH="${INSTALL_DIR}/lib/python":${PYTHONPATH}
 
     echo "Testing NEURON import ...."
-    python -c 'import neuron' >testimport.log 2>&1
+    ${PYTHON_BIN} -c 'import neuron' >testimport.log 2>&1
 
     touch -f ${INSTALL_DIR}/.install_finished
     echo "NEURON successfully installed"
