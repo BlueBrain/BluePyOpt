d02e51149acab98469ab040663a2c37c6e255a55
Tanguy Damart <tanguy.damart@epfl.ch>
2020-12-15 12:12:21 +0100

    Added NrnSecSomaDistance class to locations

--- bluepyopt/ephys/locations.py
+++ bluepyopt/ephys/locations.py
@@ -259,6 +259,7 @@ class NrnSomaDistanceCompLocation(Location, DictMixin):
     """Compartment at distance from soma"""
 
     SERIALIZED_FIELDS = ('name', 'comment', 'soma_distance', 'seclist_name', )
+    max_diam = 0.0
 
     def __init__(
             self,
@@ -298,7 +299,7 @@ class NrnSomaDistanceCompLocation(Location, DictMixin):
 
                 comp_diam = isec(comp_x).diam
 
-                if comp_diam > 0.0:
+                if comp_diam > self.max_diam:
                     icomp = isec(comp_x)
 
         if icomp is None:
