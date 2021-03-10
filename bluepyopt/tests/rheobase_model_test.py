e7c3526aaed0b27b5d0e36997e99a5ac4a796dc3
Russell Jarvis <russelljarvis@protonmail.com>
2021-03-10 14:46:55 +1100

    update for CI unittesting passing

--- bluepyopt/tests/rheobase_model_test.py
+++ bluepyopt/tests/rheobase_model_test.py
@@ -11,10 +11,9 @@ from neuronunit.optimization.model_parameters import (
     to_bpo_param
 )
 from neuronunit.optimization.optimization_management import (
-    dtc_to_rheo,
+    model_to_rheo,
     inject_and_plot_model
 )
-from neuronunit.optimization.data_transport_container import DataTC
 from jithub.models import model_classes
 import matplotlib.pyplot as plt
 import quantities as qt
@@ -33,28 +32,16 @@ class testOptimization(unittest.TestCase):
         if model_type == "IZHI":
             from jithub.models.model_classes import IzhiModel
             cellmodel = IzhiModel()
-        #    model = model_classes.IzhiModel()
-        #if cellmodel == "MAT":
-        #    model = model_classes.MATModel()
         if model_type == "ADEXP":
             from jithub.models.model_classes import ADEXPModel
             cellmodel = ADEXPModel()
 
-        #    model = model_classes.ADEXPModel()
-
-        #dtc = DataTC(backend=cellmodel)
-        #assert dtc.backend == cellmodel
         cellmodel.params = {k: np.mean(v) for k, v in MODEL_PARAMS[model_type].items()}
-        #other_params = BPO_PARAMS[cellmodel]
-        cellmodel = dtc_to_rheo(cellmodel)
-        print(cellmodel.rheobase)
-        assert cellmodel.rheobase is not None
+        cellmodel = model_to_rheo(cellmodel)
+        #assert cellmodel.rheobase is not None
         self.assertIsNotNone(cellmodel.rheobase)
         vm, plt, cellmodel = inject_and_plot_model(cellmodel, plotly=False)
         self.assertIsNotNone(vm)
-		#nt.assert_is_instance(vm, np.sum(fitnesses))
-
-
         self.assertIsNotNone(cellmodel)
 
 
