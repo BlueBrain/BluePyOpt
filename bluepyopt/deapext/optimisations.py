307514e2c5dee30cc65b3aafab4cd25ffaeb30d9
Jaquier Aurélien Tristan <aurelien.jaquier@epfl.ch>
2021-03-11 10:58:52 +0100

    Added terminator to gracefully exit optimisation.

--- bluepyopt/deapext/optimisations.py
+++ bluepyopt/deapext/optimisations.py
@@ -252,7 +252,8 @@ class DEAPOptimisation(bluepyopt.optimisations.Optimisation):
             continue_cp=False,
             cp_filename=None,
             cp_frequency=1,
-            parent_population=None):
+            parent_population=None,
+            terminator=None):
         """Run optimisation"""
         # Allow run function to override offspring_size
         # TODO probably in the future this should not be an object field
@@ -307,7 +308,8 @@ class DEAPOptimisation(bluepyopt.optimisations.Optimisation):
             halloffame=self.hof,
             cp_frequency=cp_frequency,
             continue_cp=continue_cp,
-            cp_filename=cp_filename)
+            cp_filename=cp_filename,
+            terminator=terminator)
 
         # Update hall of fame
         self.hof = hof
