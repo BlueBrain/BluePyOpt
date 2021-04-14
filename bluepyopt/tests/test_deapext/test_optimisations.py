f365eb8bc97292bb99246c8d998d9566a6f3abc1
Werner Van Geit <werner.vangeit@epfl.ch>
2021-04-14 15:51:16 +0200

    Fix simplecell in test_optimizations

--- bluepyopt/tests/test_deapext/test_optimisations.py
+++ bluepyopt/tests/test_deapext/test_optimisations.py
@@ -3,7 +3,7 @@
 import nose.tools as nt
 
 import bluepyopt.optimisations
-import bluepyopt.ephys.examples as examples
+import bluepyopt.ephys.examples.simplecell
 
 from nose.plugins.attrib import attr
 
@@ -14,8 +14,9 @@ import deap.tools
 def test_DEAPOptimisation_constructor():
     "deapext.optimisation: Testing constructor DEAPOptimisation"
 
+    simplecell = bluepyopt.ephys.examples.simplecell.SimpleCell()
     optimisation = bluepyopt.deapext.optimisations.DEAPOptimisation(
-        examples.simplecell.cell_evaluator, map_function=map)
+        simplecell.cell_evaluator, map_function=map)
 
     nt.assert_is_instance(
         optimisation,
@@ -27,7 +28,7 @@ def test_DEAPOptimisation_constructor():
     nt.assert_raises(
         ValueError,
         bluepyopt.deapext.optimisations.DEAPOptimisation,
-        examples.simplecell.cell_evaluator,
+        simplecell.cell_evaluator,
         selector_name='wrong')
 
 
@@ -35,8 +36,9 @@ def test_DEAPOptimisation_constructor():
 def test_IBEADEAPOptimisation_constructor():
     "deapext.optimisation: Testing constructor IBEADEAPOptimisation"
 
+    simplecell = bluepyopt.ephys.examples.simplecell.SimpleCell()
     optimisation = bluepyopt.deapext.optimisations.IBEADEAPOptimisation(
-        examples.simplecell.cell_evaluator, map_function=map)
+        simplecell.cell_evaluator, map_function=map)
 
     nt.assert_is_instance(
         optimisation,
@@ -47,8 +49,9 @@ def test_IBEADEAPOptimisation_constructor():
 def test_DEAPOptimisation_run():
     "deapext.optimisation: Testing DEAPOptimisation run"
 
+    simplecell = bluepyopt.ephys.examples.simplecell.SimpleCell()
     optimisation = bluepyopt.optimisations.DEAPOptimisation(
-        examples.simplecell.cell_evaluator, offspring_size=1)
+        simplecell.cell_evaluator, offspring_size=1)
 
     pop, hof, log, hist = optimisation.run(max_ngen=1)
 
@@ -64,8 +67,9 @@ def test_DEAPOptimisation_run():
 def test_DEAPOptimisation_run_from_parents():
     "deapext.optimisation: Testing DEAPOptimisation run using prior parents"
 
+    simplecell = bluepyopt.ephys.examples.simplecell.SimpleCell()
     optimisation = bluepyopt.optimisations.DEAPOptimisation(
-        examples.simplecell.cell_evaluator, offspring_size=1)
+        simplecell.cell_evaluator, offspring_size=1)
 
     parent_population = [[0.060, 0.065]]
     pop, hof, log, hist = optimisation.run(max_ngen=0,
@@ -79,22 +83,28 @@ def test_DEAPOptimisation_run_from_parents():
 def test_selectorname():
     "deapext.optimisation: Testing selector_name argument"
 
+    simplecell = bluepyopt.ephys.examples.simplecell.SimpleCell()
+
     # Test default value
     ibea_optimisation = bluepyopt.optimisations.DEAPOptimisation(
-        examples.simplecell.cell_evaluator)
+        simplecell.cell_evaluator)
     nt.assert_equal(ibea_optimisation.selector_name, 'IBEA')
+
+    simplecell = bluepyopt.ephys.examples.simplecell.SimpleCell()
 
     # Test NSGA2 selector
     nsga2_optimisation = bluepyopt.optimisations.DEAPOptimisation(
-        examples.simplecell.cell_evaluator, selector_name='NSGA2')
+        simplecell.cell_evaluator, selector_name='NSGA2')
 
     nt.assert_equal(
         nsga2_optimisation.toolbox.select.func,
         deap.tools.emo.selNSGA2)
 
+    simplecell = bluepyopt.ephys.examples.simplecell.SimpleCell()
+
     # Test IBEA selector
     ibea_optimisation = bluepyopt.optimisations.DEAPOptimisation(
-        examples.simplecell.cell_evaluator, selector_name='IBEA')
+        simplecell.cell_evaluator, selector_name='IBEA')
 
     nt.assert_equal(
         ibea_optimisation.toolbox.select.func,
