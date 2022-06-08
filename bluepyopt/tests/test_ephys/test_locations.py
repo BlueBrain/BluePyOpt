b0515e23713cc5f5df330ccddef17226790a57e2
arnaudon <alexis.arnaudon@epfl.ch>
2022-06-08 11:53:34 +0200

    test

--- bluepyopt/tests/test_ephys/test_locations.py
+++ bluepyopt/tests/test_ephys/test_locations.py
@@ -21,7 +21,7 @@ Copyright (c) 2016-2022, EPFL/Blue Brain Project
 
 # pylint:disable=W0612, W0201
 import json
-
+import numpy as np
 
 import pytest
 
@@ -270,6 +270,77 @@ class TestNrnSecSomaDistanceCompLocation(object):
         assert comp == dend2(0.5)
         comp = self.loc_other.instantiate(sim=self.sim, icell=cell)
         assert comp == dend4(0.5)
+
+
+@pytest.mark.unit
+class TestNrnTrunkSomaDistanceCompLocation(object):
+
+    """Test class for NrnTrunkSomaDistanceCompLocation"""
+
+    def setup(self):
+        """Setup"""
+        self.loc = ephys.locations.NrnTrunkSomaDistanceCompLocation(
+            'test',
+            soma_distance=150,
+            sec_name='testdend')
+        self.loc_other = ephys.locations.NrnTrunkSomaDistanceCompLocation(
+            'test',
+            soma_distance=350,
+            sec_name='testdend')
+
+        assert self.loc.name == 'test'
+        self.sim = ephys.simulators.NrnSimulator()
+
+    def test_instantiate(self):
+        """ephys.locations.NrnSomaDistanceCompLocation: test instantiate"""
+
+        # Create a little test class with a soma and two dendritic sections
+        class Cell(object):
+
+            """Cell class"""
+            pass
+        cell = Cell()
+        soma = self.sim.neuron.h.Section(name="soma[0]")
+        cell.soma = [soma]
+        cell.testdend = self.sim.neuron.h.SectionList()
+        dend1 = self.sim.neuron.h.Section(name='dend[0]')
+        dend2 = self.sim.neuron.h.Section(name='dend[1]')
+        dend3 = self.sim.neuron.h.Section(name='dend[2]')
+        dend4 = self.sim.neuron.h.Section(name='dend[3]')
+        dend5 = self.sim.neuron.h.Section(name='dend[4]')
+
+        cell.testdend.append(sec=dend1)
+        cell.testdend.append(sec=dend2)
+        cell.testdend.append(sec=dend3)
+        cell.testdend.append(sec=dend4)
+        cell.testdend.append(sec=dend5)
+
+
+        x0 = self.sim.neuron.h.Vector([0] * 10)
+        d = self.sim.neuron.h.Vector([1] * 10)
+
+        x1 = self.sim.neuron.h.Vector(np.linspace(0, 100, 10))
+        self.sim.neuron.h.pt3dadd(x0, x1, x0, d, sec=dend1)
+        x2 = self.sim.neuron.h.Vector(np.linspace(100, 200, 10))
+        self.sim.neuron.h.pt3dadd(x0, x2, x0, d, sec=dend2)
+        x3 = self.sim.neuron.h.Vector(np.linspace(200, 300, 10))
+        self.sim.neuron.h.pt3dadd(x0, x3, x0, d, sec=dend3)
+        x4 = self.sim.neuron.h.Vector(np.linspace(300, 400, 10))
+        self.sim.neuron.h.pt3dadd(x0, x4, x0, d, sec=dend4)
+        x5 = self.sim.neuron.h.Vector(np.linspace(400, 500, 10))
+        self.sim.neuron.h.pt3dadd(x0, x5, x0, d, sec=dend5)
+
+        dend1.connect(soma(0.5), 0.0)
+        dend2.connect(dend1(1.0), 0.0)
+        dend3.connect(dend1(1.0), 0.0)
+        dend4.connect(dend3(1.0), 0.0)
+        dend5.connect(dend4(1.0), 0.0)
+
+        comp = self.loc.instantiate(sim=self.sim, icell=cell)
+        assert comp == dend3(0.5)
+        comp = self.loc_other.instantiate(sim=self.sim, icell=cell)
+        assert comp == dend5(0.5)
+
 
 
 @pytest.mark.unit
