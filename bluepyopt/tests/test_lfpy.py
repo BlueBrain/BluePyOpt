cc39eef32a47db1e3b0614221275f1b97aad6941
Jaquier Aurélien Tristan <aurelien.jaquier@epfl.ch>
2022-07-29 14:02:05 +0200

    Fix lfpy evaluator and lfpy test

--- bluepyopt/tests/test_lfpy.py
+++ bluepyopt/tests/test_lfpy.py
@@ -23,7 +23,8 @@ def test_lfpy_evaluator():
     """Test CellEvaluator with an LFPy cell and LFPy simulator"""
 
     evaluator = l5pc_lfpy_evaluator.create(
-        feature_file=L5PC_LFPY_PATH + "/extra_features.json"
+        feature_file=L5PC_LFPY_PATH + "/extra_features.json",
+        cvode_active=False,
     )
 
     responses = evaluator.run_protocols(
@@ -33,5 +34,5 @@ def test_lfpy_evaluator():
     values = evaluator.fitness_calculator.calculate_values(responses)
 
     assert len(values) == 21
-    assert abs(values['Step1.soma.AP_height'] - 28.1141131972856) < 1e-5
+    assert abs(values['Step1.soma.AP_height'] - 27.85963902931001) < 1e-5
     assert len(responses['Step1.MEA.v']["voltage"]) == 40
