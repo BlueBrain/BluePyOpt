2ab0c58b3d882b2c00d626a57e4d612e19279711
Alessio Buccino <alejoe9187@gmail.com>
2022-08-23 10:49:52 +0200

    Install LFPy from master (no MPI needed)

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ EXTRA_SCOOP = [
 ]
 
 EXTRA_LFP = [
-    'LFPy @ git+https://github.com/LFPy/LFPy.git@mpi_optional',
+    'LFPy @ git+https://github.com/LFPy/LFPy.git@master',
 ]
 
 setuptools.setup(
