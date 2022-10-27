9a30acdf4d8c6cf96b5d4d8269c9e691caea39b3
Anil Tuncel <anil.tuncel@epfl.ch>
2022-10-27 18:20:53 +0200

    add unit tests for acc_label

---
+++ bluepyopt/tests/test_ephys/test_acc/test_acc_label.py
@@ -0,0 +1,20 @@
+"""Unit tests for acc_label.py."""
+
+from bluepyopt.ephys.acc.acc_label import ArbLabel
+
+def test_arb_label():
+    """Test ArbLabel class."""
+    _type = "region"
+    name = "all"
+    defn = "(all)"
+
+    arb = ArbLabel(type=_type, name=name, defn=defn)
+
+    assert arb.defn == '(%s-def "%s" %s)' % (_type, name, defn)
+    assert arb.ref == '(%s "%s")' % (_type, name)
+    assert arb.name == name
+    assert arb.loc == defn
+    assert arb == arb
+    assert arb != None
+    assert hash(arb) == hash(arb.defn)
+    assert repr(arb) == arb.defn
