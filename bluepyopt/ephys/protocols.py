120b1e93d9e60070100271ce360393ecb08c32a1
Tanguy Pierre Louis Damart <tanguy.damart@epfl.ch>
2023-06-05 15:29:04 +0200

    Add warning to windows case

--- bluepyopt/ephys/protocols.py
+++ bluepyopt/ephys/protocols.py
@@ -268,6 +268,12 @@ class SweepProtocol(Protocol):
             # Spwan context would reset NEURON properties, such as dt.
             if sys.platform == 'win32':
                 multiprocessing_context = multiprocessing.get_context('spawn')
+                if (
+                    sim is not None and not sim.cvode_active and
+                    sim.dt != 0.025
+                ):
+                    logger.warning("On Windows, evaluation might break when"
+                                   "using non-default fixed time steps.")
             else:
                 multiprocessing_context = multiprocessing.get_context('fork')
 
