1a78f86e46835da0e67db91f407824571d2363b3
Werner Van Geit <werner.vangeit@epfl.ch>
2024-03-22 10:35:18 +0100

    Replace setup() with setup_method() in func tests

--- bluepyopt/tests/test_simplecell.py
+++ bluepyopt/tests/test_simplecell.py
@@ -1,40 +1,38 @@
 """Simple cell example test class"""
 
-import sys
 import os
+import sys
 
-SIMPLECELL_PATH = os.path.abspath(os.path.join(
-    os.path.dirname(__file__),
-    '../../examples/simplecell'))
+SIMPLECELL_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../examples/simplecell")
+)
 
 # sys.path.insert(0, SIMPLECELL_PATH)
 
 
 class TestSimpleCellClass(object):
-
     """Simple cell example test class for NEURON"""
 
-    def setup(self):
+    def setup_method(self):
         """Setup"""
         self.old_cwd = os.getcwd()
         self.old_stdout = sys.stdout
 
         os.chdir(SIMPLECELL_PATH)
-        sys.stdout = open(os.devnull, 'w')
+        sys.stdout = open(os.devnull, "w")
 
-    @staticmethod
-    def test_exec():
+    def test_exec(self):
         """Simplecell NEURON: test execution"""
         # When using import instead of execfile this doesn't work
         # Probably because multiprocessing doesn't work correctly during
         # import
         if sys.version_info[0] < 3:
-            execfile('simplecell.py')  # NOQA
+            execfile("simplecell.py")  # NOQA
         else:
-            with open('simplecell.py') as sc_file:
-                exec(compile(sc_file.read(), 'simplecell.py', 'exec'))  # NOQA
+            with open("simplecell.py") as sc_file:
+                exec(compile(sc_file.read(), "simplecell.py", "exec"))  # NOQA
 
-    def teardown(self):
+    def teardown_method(self):
         """Tear down"""
 
         sys.stdout = self.old_stdout
@@ -42,31 +40,28 @@ class TestSimpleCellClass(object):
 
 
 class TestSimpleCellArborClass(object):
-
     """Simple cell example test class for Arbor"""
 
-    def setup(self):
+    def setup_method(self):
         """Setup"""
         self.old_cwd = os.getcwd()
         self.old_stdout = sys.stdout
 
         os.chdir(SIMPLECELL_PATH)
-        sys.stdout = open(os.devnull, 'w')
+        sys.stdout = open(os.devnull, "w")
 
-    @staticmethod
-    def test_exec():
+    def test_exec(self):
         """Simplecell Arbor: test execution"""
         # When using import instead of execfile this doesn't work
         # Probably because multiprocessing doesn't work correctly during
         # import
         if sys.version_info[0] < 3:
-            execfile('simplecell_arbor.py')  # NOQA
+            execfile("simplecell_arbor.py")  # NOQA
         else:
-            with open('simplecell_arbor.py') as sc_file:
-                exec(compile(sc_file.read(),
-                    'simplecell_arbor.py', 'exec'))  # NOQA
+            with open("simplecell_arbor.py") as sc_file:
+                exec(compile(sc_file.read(), "simplecell_arbor.py", "exec"))  # NOQA
 
-    def teardown(self):
+    def teardown_method(self):
         """Tear down"""
 
         sys.stdout = self.old_stdout
